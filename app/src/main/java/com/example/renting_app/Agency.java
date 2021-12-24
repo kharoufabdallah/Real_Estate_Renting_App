ec3b8cf42b06bd8002731011528c107cd84c03ba
kharoufabdallah <48812005+kharoufabdallah@users.noreply.github.com>
2021-12-24 14:28:56 +0200

    classes1

--- app/src/main/java/com/example/renting_app/Agency.java
+++ app/src/main/java/com/example/renting_app/Agency.java
@@ -1,5 +1,82 @@
 package com.example.renting_app;
 
 public class Agency {
+    String agency_email;
+    String agency_name;
+    String password;
+    String countyr;
+    String city;
+    String agency_phone;
 
+    public Agency() {
+    }
+
+    public Agency(String agency_email, String agency_name, String password, String countyr, String city, String agency_phone) {
+        this.agency_email = agency_email;
+        this.agency_name = agency_name;
+        this.password = password;
+        this.countyr = countyr;
+        this.city = city;
+        this.agency_phone = agency_phone;
+    }
+
+    public String getAgency_email() {
+        return agency_email;
+    }
+
+    public void setAgency_email(String agency_email) {
+        this.agency_email = agency_email;
+    }
+
+    public String getAgency_name() {
+        return agency_name;
+    }
+
+    public void setAgency_name(String agency_name) {
+        this.agency_name = agency_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountyr() {
+        return countyr;
+    }
+
+    public void setCountyr(String countyr) {
+        this.countyr = countyr;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAgency_phone() {
+        return agency_phone;
+    }
+
+    public void setAgency_phone(String agency_phone) {
+        this.agency_phone = agency_phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Agency{" +
+                "agency_email='" + agency_email + '\'' +
+                ", agency_name='" + agency_name + '\'' +
+                ", password='" + password + '\'' +
+                ", countyr='" + countyr + '\'' +
+                ", city='" + city + '\'' +
+                ", agency_phone='" + agency_phone + '\'' +
+                '}';
+    }
 }
---
+++ app/src/main/java/com/example/renting_app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.renting_app;
+//import org.apache.commons.validator.routines.EmailValidator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+//    public static boolean isValidEmail(String email) {
+//        // create the EmailValidator instance
+//        EmailValidator validator = EmailValidator.getInstance();
+//
+//        // check for valid email addresses using isValid method
+//        return validator.isValid(email);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
