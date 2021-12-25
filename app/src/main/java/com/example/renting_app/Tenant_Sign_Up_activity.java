9dfe7cc3e19dc9982c428b2988d7e5fa5d2bdec0
kharoufabdallah <48812005+kharoufabdallah@users.noreply.github.com>
2021-12-25 20:48:18 +0200

    complete signup

---
+++ app/src/main/java/com/example/renting_app/Tenant_Sign_Up_activity.java
@@ -0,0 +1,14 @@
+package com.example.renting_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tenant_Sign_Up_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant__sign__up);
+    }
+}
