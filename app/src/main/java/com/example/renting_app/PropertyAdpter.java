5ebbfac9bbde36dc32814c902c86eff032c54c6b
kharoufabdallah <48812005+kharoufabdallah@users.noreply.github.com>
2021-12-31 19:09:50 +0200

    home ready => complete insert prop and fix databse of props

--- app/src/main/java/com/example/renting_app/PropertyAdpter.java
+++ app/src/main/java/com/example/renting_app/PropertyAdpter.java
@@ -36,13 +36,19 @@ public class PropertyAdpter extends RecyclerView.Adapter<PropertyAdpter.ViewHold
         holder.pro_name_tv.setText(prop_list.getCity()); // no name of property - replaced by city
         holder.pro_date_tv.setText(Integer.toString(prop_list.getConst_year())); // as a date
         holder.imgview.setImageResource(prop_list.getImage_view());
+        holder.pro_stat_tv.setText(prop_list.getStatus());
+        holder.pro_bed_tv.setText(Integer.toString(prop_list.getBedroom_no()));
+        holder.pro_surf_tv.setText(Double.toString(prop_list.getSurface_area()));
+        holder.pro_price_tv.setText(Double.toString(prop_list.getRental_price()));
+
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, prop_list.getStatus(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Application is going to be opened ", Toast.LENGTH_SHORT).show();
                 // list of prop added for instance statically just to test ya man
-             //   here is the actual work of each prop in hte list -- when click on the card item
+               //here is the actual work of each prop in hte list -- when click on the card item
+                application_form_of_property();
             }
         });
     }
@@ -56,6 +62,10 @@ public class PropertyAdpter extends RecyclerView.Adapter<PropertyAdpter.ViewHold
         ImageView imgview;
         TextView pro_name_tv;
         TextView pro_date_tv;
+        TextView pro_surf_tv;
+        TextView pro_bed_tv;
+        TextView pro_stat_tv;
+        TextView pro_price_tv;
 
 
         public ViewHolder(@NonNull View itemView) {
@@ -63,7 +73,15 @@ public class PropertyAdpter extends RecyclerView.Adapter<PropertyAdpter.ViewHold
             imgview= itemView.findViewById(R.id.imageview_home);
             pro_date_tv= itemView.findViewById(R.id.textdate_home);
             pro_name_tv= itemView.findViewById(R.id.textName_home);
-
+            pro_surf_tv= itemView.findViewById(R.id.textSurfaceArea_home);
+            pro_bed_tv=itemView.findViewById(R.id.textViewBedNo_home);
+            pro_stat_tv=itemView.findViewById(R.id.textViewStatus_home);
+            pro_price_tv=itemView.findViewById(R.id.textViewRentalPrice_home);
         }
+    }
+
+    public void  application_form_of_property()
+    {
+
     }
 }
